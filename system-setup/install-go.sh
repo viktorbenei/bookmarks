4f6c7919bcdd9e6bf187b71f2dc9dfecd0653fcd
Viktor Benei <viktor.benei@gmail.com>
2019-12-24 13:12:56 +0100

    install-go.sh script

---
+++ system-setup/install-go.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#
+# Usage example: env GO_VERSION='1.13.5' ./system-setup/install-go.sh
+#
+
+set -ex
+
+OS_ID=''
+case "$OSTYPE" in
+  darwin*)  OS_ID='darwin' ;;
+  linux*)   OS_ID='linux' ;;
+esac
+if [ -z "$OS_ID" ]; then echo "Unsupported OS: $OSTYPE"; exit 1; fi
+
+ARCH_ID=''
+archs=`uname -m`
+case "$archs" in
+  x86_64)  ARCH_ID='amd64' ;;
+esac
+
+if [ -z "$GO_VERSION" ]; then echo "No GO_VERSION" specified ; exit 1; fi
+
+
+go_version_tar_gz="go${GO_VERSION}.${OS_ID}-${ARCH_ID}.tar.gz"
+
+cd /tmp
+curl -O "https://storage.googleapis.com/golang/${go_version_tar_gz}"
+sudo tar -C /usr/local -xzf "${go_version_tar_gz}"
