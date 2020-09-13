42e419d69f30c87b1b7c471b2cfd8eea52c6c925
Viktor Benei <viktorbenei@bitrise.io>
2020-09-13 16:23:59 +0000

    updated ubuntu setup.sh

--- system-setup/ubuntu/setup.sh
+++ system-setup/ubuntu/setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
+THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 sudo apt-get update
 sudo apt-get upgrade -y
 
@@ -12,29 +14,35 @@ sudo apt-get install -y \
   fonts-hack
 
 # Snap packages
-sudo snap install \
-  vlc keepassxc communitheme
+sudo snap install vlc
+sudo snap install communitheme
 
 # Fish
-sudo apt-add-repository -y ppa:fish-shell/release-2
-sudo apt-get update
-sudo apt-get install -y fish
-# Make it the default shell
-if [[ "$SHELL" != "/usr/bin/fish" ]] ; then
-  chsh -s /usr/bin/fish
-fi
-# Setup oh-my-fish
-if [ ! -d "$HOME/.local/share/omf" ] ; then
-  curl -L https://get.oh-my.fish | fish
-  fish -c 'omf install bobthefish'
+if [[ "$INSTALL_FISH" == "true" ]] ; then
+  sudo apt-add-repository -y ppa:fish-shell/release-2
+  sudo apt-get update
+  sudo apt-get install -y fish
+  # Make it the default shell
+  if [[ "$SHELL" != "/usr/bin/fish" ]] ; then
+    chsh -s /usr/bin/fish
+  fi
+  # Setup oh-my-fish
+  if [ ! -d "$HOME/.local/share/omf" ] ; then
+    curl -L https://get.oh-my.fish | fish
+    fish -c 'omf install bobthefish'
+  fi
 fi
 
 # Bitrise CLI
 if ! [ -x "$(command -v bitrise)" ]; then
   echo "bitrise cli not installed - installing ..."
-  curl https://gist.githubusercontent.com/viktorbenei/8900fefd8338559e4c4ccb34fbcfe37d/raw/install-bitrise-cli.sh | bash -s 1.26.0
+  curl https://gist.githubusercontent.com/viktorbenei/8900fefd8338559e4c4ccb34fbcfe37d/raw/install-bitrise-cli.sh | bash -s 1.42.0
 else
   echo "bitrise cli already installed"
+fi
+
+if [[ "$INSTALL_GO" == "true" ]] ; then
+  env GO_VERSION="1.15.2" bash "${THIS_SCRIPTDIR}/../install-go.sh"
 fi
 
 echo '=== FINISHED [OK] ==='
