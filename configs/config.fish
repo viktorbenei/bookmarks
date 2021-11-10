eef3618f84d753eb92aa4136138ec4db22013b1e
Viktor Benei <viktorbenei@bitrise.io>
2021-11-10 13:54:47 +0100

    fish theme configs

--- configs/config.fish
+++ configs/config.fish
@@ -1,3 +1,12 @@
 # ~/.config/fish/config.fish
 
+# fish configs
+# bobthefish theme configs:
+set -g theme_display_user no
+set -g theme_display_hostname no
+set -g theme_display_ruby no
+set -g theme_display_node no
+
+# Go
+# Add go bin dir to PATH
 set -x PATH $HOME/go/bin $PATH
